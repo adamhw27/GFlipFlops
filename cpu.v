2ec58cee6b06c3d4adc37d03cc26232fd74868f7
harrisonCompE <harrison.letourneau@gmail.com>
2025-09-10 16:15:54 -0600

    CPU and MUX new
    RegFile has been connected to ALU via mux, and ALU connected back into RegFile

---
+++ cpu.v
@@ -0,0 +1,54 @@
+module cpu (
+input [7:0] opcode,
+input cin,
+input clck, reset,
+
+output [27:0] segOut,
+output [4:0] flags
+);
+
+// Make registers
+wire [15:0] r0, r1, r2, r3, r4, r5, r6, r7, r8 ,r9, r10, r11, r12, r13, r14, r15;
+
+// Connect R1 and R2 alu
+wire [15:0] R1;
+wire [15:0] R2;
+
+// Connect regEnable to RegFile
+wire [15:0] regEnable;
+
+//assign R2 = 16'h7FFF;
+//assign R1 = 16'h0001;
+
+// To connect to Bus
+wire [15:0] aluOut;
+
+// To connect mux to FSM
+wire [3:0] srcSel;
+wire [3:0] dstSel;
+
+//Instantiating the ALU
+alu alu(.R1(R1), .R2(R2), .opcode(opcode), .aluOut(aluOut), .flags(flags), .cin(cin));
+
+// Connect Alu out to Reg Bank
+RegBank rb(.ALUBus(aluOut), .r0(r0), .r1(r1), .r2(r2), .r3(r3), .r4(r4), .r5(r5), .r6(r6),
+					.r7(r7), .r9(r9), .r10(r10), .r11(r11), .r12(r12), .r13(r13), .r14(r14), .r15(r15), .regEnable(regEnable), .clk(clk), .reset(reset));
+					
+					
+// Connect Rsrc to ALU via Mux
+
+Mux RSrcMux(.r0(r0), .r1(r1), .r2(r2), .r3(r3), .r4(r4), .r5(r5), .r6(r6), .r7(r7), .r9(r9), 
+					.r10(r10), .r11(r11), .r12(r12), .r13(r13), .r14(r14), .r15(r15), .sel(srcSel), .out(R1));
+					
+// Connect Rdst to ALU via Mux
+
+Mux RDstMux(.r0(r0), .r1(r1), .r2(r2), .r3(r3), .r4(r4), .r5(r5), .r6(r6), .r7(r7), .r9(r9), 
+					.r10(r10), .r11(r11), .r12(r12), .r13(r13), .r14(r14), .r15(r15), .sel(dstSel), .out(R2));
+
+// Connect SegOut
+seven_seg_hex a(aluOut[3:0], segOut[6:0]);
+seven_seg_hex b(aluOut[7:4], segOut[13:7]);
+seven_seg_hex c(aluOut[11:8], segOut[20:14]);
+seven_seg_hex d(aluOut[15:12], segOut[27:21]);
+
+endmodule
