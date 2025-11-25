56b826dc90cc03818a538a5f9111b489b0878c0c
AdamW <adamwelsh.college@gmail.com>
2025-11-24 17:13:22 -0700

    added bit gen, maybe fixed counter latch issues AND FIXED vga controller

--- CPU/VGAcontroller.v
+++ CPU/VGAcontroller.v
@@ -1,96 +1,116 @@
 module VGAcontroller(
 	input wire clk, rst,
 	
-	output wire vga_clk, hs, vs, bright,
-	output wire [9:0] x, y
+	output reg hs, vs, 
+	output wire bright,
+	output reg [9:0] hCount, vCount
 );
 
 
 wire new_clk;
-wire [15:0] count;
+reg [15:0] count;
 
 clk_div divider(.clk(clk), .rst(rst), .oclk(new_clk));
 
-always @(posedge new_clk, negedge rst) begin
-	if( ~rst)begin
-		x <= 10'b0;
-		y <= 10'b0;
-		count <= 16'b0;
-	end
-	count <= count + 1;
+
+// hreset and vreset are active high
+wire [18:0] hlencount, vlencount;
+
+// hreset is set high once per line, so it is the clock at which vlencount should increment
+counter_19bit hcounter(.clk(count), .rst(hreset), .val(hlencount));
+counter_19bit vcounter(.clk(hreset), .rst(vreset), .val(vlencount));
+
+reg hbright, vbright, hreset, vreset;
+
+always @(posedge new_clk) begin
+	count <= count + 1'b1;
 end
 
 
 // need to count the number of count signals 
 
-wire [9:0] hlencount;
-wire [9:0] vlencount;
 
-wire hbright;
-wire vbright;
-vlencount = 10'b0;
-hlencount = 10'b0;
 
-always @(count, hlencount) begin
-
-	if (count == 0)
-		hlencount = 0;
+always @(hlencount, hCount) 
+begin
+	hreset <= 1'b0;
 		
-	
-	if (hlencount < 96)begin
-		hs = 0;
-		hbright = 0;
+	if (hlencount < 96)
+	begin
+		hs <= 0;
+		hbright <= 0;
+		hCount <= 0;
 	end
-	else begin
-		hs = 1;
+	else 
+	begin
+		hs <= 1;
 		if (hlencount < 144)
-			hbright = 0;
-		else begin
+		begin
+			hbright <= 0;
+			hCount <= 0;
+		end
+		else 
+		begin
 			if (hlencount < 784)
-				hbright = 1;
+			begin
+				hCount <= hCount + 1'b1;
+				hbright <= 1;
+			end
+			else if (hlencount < 800)
+			begin
+				hCount <= 0;
+				hbright <= 0;
+			end
 			else
-				hbright = 0;
+			begin
+				hbright <= 0;
+				hCount <= 10'b0;
+				hreset <= 1'b1;
 			end
 		end
-	end
-	
-	hlencount <= hlencount + 1;
-	
-	if (hlencount > 800)begin
-		hlencount = 0;
 	end
 end
 
 
-always @(count) begin
+always @(vlencount, vCount) 
+begin
+	vreset <= 1'b0;
 
-	if (vlencount < 1600)begin
-		vs = 0;
-		vbright = 0;
+	if (vlencount < 2)
+	begin
+		vs <= 0;
+		vbright <= 0;
+		vCount <= 0;
 	end
-	else begin
-		hs = 1;
-		if (vlencount < 24800)
-			vbright = 0;
-		else begin
-			if (vlencount < 408800)
-				vbright = 1;
+	else 
+	begin
+		vs <= 1;
+		if (vlencount < 35)
+		begin
+			vCount <= 0;
+			vbright <= 0;
+		end
+		else 
+		begin
+			if (vlencount < 515)
+			begin
+				vCount <= vCount + 1'b1;
+				vbright <= 1;
+			end
+			else if (vlencount < 525)
+			begin
+				vCount <= 0;
+				vbright <= 0;
+			end
 			else
-				vbright = 0;
+			begin
+				vbright <= 0;
+				vCount <= 0;
+				vreset <= 1'b1;
 			end
 		end
 	end
-	
-	vlencount <= vlencount + 1;
-	
-	if (vlencount > 416800)begin
-		vlencount <= 0;
-	end
 end
 
-
-bright = vbright & hbright;
-
-
-
+assign bright = vbright & hbright;
 endmodule
