8f02ea3d5669cac9d03eec96f3ac65b72d1dcb04
AdamW <adamwelsh.college@gmail.com>
2025-11-25 15:36:15 -0700

    added hardcoded vga

---
+++ CPU/HardCodedVga.v
@@ -0,0 +1,12 @@
+module HardCodedVga(
+	input clk, rst,
+	output vga_clk, vga_blank_n, vga_vs, vga_hs,
+	output [7:0] rgb
+);
+	
+	wire[9:0] hCount, vCount;
+	
+	VGAcontroller vga_control(.clk(clk), .rst(rst), .hs(vga_hs), .vs(vga_vs), .bright(vga_blank_n), .hCount(hCount), .vCount(vCount));
+	VGABitGen vga_bitgen(.bright(vga_blank_n), .hCount(hCount), .vCount(vCount), .rgb(rgb));
+	
+endmodule
